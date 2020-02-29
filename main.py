4a67e2364ba3980d895a4856710697e10063c4a4
mtellezgarc <61028574+mtellezgarc@users.noreply.github.com>
2020-02-28 23:54:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+
 #Primero y segundo programa
 
 import time
 from random import randint
+listaUno=[];
+listaDos=[];
+listaTres=[];
+
 #Leer archivo
 f=open("mil.txt","r")
 tiempo=time.time()
@@ -12,15 +17,16 @@ con=0
 #Número aleatorio entre 1 y 4
 numero=randint(1,4)
 for x in f:
+  listaUno.append(int(x));
 #If para si el número aleatorio coincide con alguno del archivo
 #Int() convierte la x en un número entero para que pueda comparar
   if numero == int(x):
     #Si encuentra el contador cuenta uno y si encuentra mas #números
     con=con+1
   suma += int(x)
-print("Mil datos")
+print("Ejercicio 1 y 2 archivo Mil datos")
 print("Suma = "+str(suma))
-print("Duracion "+str(time.time()-tiempo)+" segundos")
+print("Duración "+str(time.time()-tiempo)+" segundos")
 print("Número a validar: "+str(numero))
 print("Número de veces q se repite en el archivo: "+str(con))
 print("\n")
@@ -35,15 +41,16 @@ con=0
 #Número aleatorio entre 1 y 4
 numero=randint(1,4)
 for x in f:
+  listaDos.append(int(x));
 #If para si el número aleatorio coincide con alguno del archivo
 #Int() convierte la x en un número entero para que pueda comparar
   if numero == int(x):
     #Si encuentra el contador cuenta uno y si encuentra mas #números
     con=con+1
   suma += int(x)
-print("Diez mil datos")
+print("Ejercicio 1 y 2 archivo Diez mil datos")
 print("Suma = "+str(suma))
-print("Duracion "+str(time.time()-tiempo2)+" segundos")
+print("Duración "+str(time.time()-tiempo2)+" segundos")
 print("Número a validar "+str(numero))
 print("Número de veces q se repite en el archivo: "+str(con))
 print("\n")
@@ -58,22 +65,25 @@ con=0
 #Número aleatorio entre 1 y 4
 numero=randint(1,4)
 for x in f:
+  listaTres.append(int(x));
+ 
 #If para si el número aleatorio coincide con alguno del archivo
 #Int() convierte la x en un número entero para que pueda comparar
   if numero == int(x):
     #Si encuentra el contador cuenta uno y si encuentra mas #números
     con=con+1
   suma += int(x)
-print("Millón de datos")
+
+print("Ejercicio 1 y 2 archivo Millón de datos")
 print("Suma = "+str(suma))
-print("Duracion "+str(time.time()-tiempo3)+" segundos")
+print("Duración "+str(time.time()-tiempo3)+" segundos")
 print("Número a validar: "+str(numero))
 print("Número de veces q se repite en el archivo: "+str(con))
 print("\n")
 
 
 #3 Torre de hunai
-print("torre de hunai")
+print("Ejercico 3 Torre de hanói")
 def torre(discos, torreUno =1, torreTres =3):
     if discos:
         torre(discos-1,torreUno,6-torreUno-torreTres)
@@ -96,8 +106,240 @@ def binary_search_re(array, x, left, right):
         return binary_search_re(array, x, mid+1, right)
 
 print("\n")
-print("Resultado de búsqueda Binario")
+print("Ejercicio 4 resultado de búsqueda Binario")
 arr = [0, 2, 3, 4, 5, 6, 7, 9, 10]
 #arr es el array, 5 por ejemplo es el número que vamos a buscar, cero es pa posicicón inicla de array, len arr -1 es el final de array
 print("El número esta en la siguiente posición del array: "
 +str(binary_search_re(arr, 5, 0, len(arr)-1)))
+print("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#lista Uno 
+
+tiempoListaUno=time.time()
+# Método de ordenamiento Burbuja
+def ordenamientoBurbuja(unaLista):
+  for numPasada in range(len(unaLista)-1,0,-1):
+    for i in range(numPasada):
+       if unaLista[i]>unaLista[i+1]:
+                temp = unaLista[i]
+                unaLista[i] = unaLista[i+1]
+                unaLista[i+1] = temp
+
+
+ordenamientoBurbuja(listaUno)
+print ("El orden en la lista por el método de la burbuja es:"+str(listaUno))
+print("\n")
+print("tiempo")
+print("duracion "+str(time.time()-tiempoListaUno));
+print("\n")
+print("\n")
+
+
+# Método de ordenamiento Inserción
+tiempoListaUno=time.time()
+def ordenamientoPorInsercion(unaLista):
+   for indice in range(1,len(unaLista)):
+
+     valorActual = unaLista[indice]
+     posicion = indice
+
+     while posicion>0 and unaLista[posicion-1]>valorActual:
+         unaLista[posicion]=unaLista[posicion-1]
+         posicion = posicion-1
+     unaLista[posicion]=valorActual
+
+ordenamientoPorInsercion(listaUno)
+print("El orden en la lista por el método de inserción es:"+str(listaUno))
+print("\n")
+print("tiempo")
+print("duracion "+str(time.time()-tiempoListaUno));
+print("\n")
+print("\n")
+
+# Método de ordenamiento Quicksort
+tiempoListaUno=time.time()
+def sort(lista):
+    izquierda = []
+    centro = []
+    derecha = []
+    if len(lista) > 1:
+        pivote = lista[0]
+        for i in lista:
+            if i < pivote:
+                izquierda.append(i)
+            elif i == pivote:
+                centro.append(i)
+            elif i > pivote:
+                derecha.append(i)
+        #print(izquierda+["-"]+centro+["-"]+derecha)
+        return sort(izquierda)+centro+sort(derecha)
+    else:
+      return lista
+
+print()
+print ("El orden en la lista por el método de Quicksort es:"+str(sort(listaUno)))
+print("tiempo")
+print("duracion "+str(time.time()-tiempoListaUno));
+print("\n")
+print("\n")
+
+
+#lista Dos
+
+
+# Método de ordenamiento Burbuja
+tiempoListaUno=time.time()
+def ordenamientoBurbuja(unaLista):
+  for numPasada in range(len(unaLista)-1,0,-1):
+    for i in range(numPasada):
+       if unaLista[i]>unaLista[i+1]:
+                temp = unaLista[i]
+                unaLista[i] = unaLista[i+1]
+                unaLista[i+1] = temp
+
+
+ordenamientoBurbuja(listaDos)
+print ("El orden en la lista por el método de la burbuja es:"+str(listaDos))
+print("\n")
+print("tiempo")
+print("duracion "+str(time.time()-tiempoListaUno));
+print("\n")
+print("\n")
+
+
+# Método de ordenamiento Inserción
+tiempoListaUno=time.time()
+def ordenamientoPorInsercion(unaLista):
+   for indice in range(1,len(unaLista)):
+
+     valorActual = unaLista[indice]
+     posicion = indice
+
+     while posicion>0 and unaLista[posicion-1]>valorActual:
+         unaLista[posicion]=unaLista[posicion-1]
+         posicion = posicion-1
+     unaLista[posicion]=valorActual
+
+ordenamientoPorInsercion(listaDos)
+print("El orden en la lista por el método de inserción es:"+str(listaDos))
+print("\n")
+print("tiempo")
+print("duracion "+str(time.time()-tiempoListaUno));
+print("\n")
+print("\n")
+
+# Método de ordenamiento Quicksort
+tiempoListaUno=time.time()
+def sort(lista):
+    izquierda = []
+    centro = []
+    derecha = []
+    if len(lista) > 1:
+        pivote = lista[0]
+        for i in lista:
+            if i < pivote:
+                izquierda.append(i)
+            elif i == pivote:
+                centro.append(i)
+            elif i > pivote:
+                derecha.append(i)
+        #print(izquierda+["-"]+centro+["-"]+derecha)
+        return sort(izquierda)+centro+sort(derecha)
+    else:
+      return lista
+
+print()
+print ("El orden en la lista por el método de Quicksort es:"+str(sort(listaDos)))
+print("tiempo")
+print("duracion "+str(time.time()-tiempoListaUno));
+print("\n")
+print("\n")
+
+#Lista Tres
+
+# Método de ordenamiento Burbuja
+tiempoListaUno=time.time()
+def ordenamientoBurbuja(unaLista):
+  for numPasada in range(len(unaLista)-1,0,-1):
+    for i in range(numPasada):
+       if unaLista[i]>unaLista[i+1]:
+                temp = unaLista[i]
+                unaLista[i] = unaLista[i+1]
+                unaLista[i+1] = temp
+
+ordenamientoBurbuja(listaTres)
+print ("El orden en la lista por el método de la burbuja es:"+str(listaTres))
+print("\n")
+print("tiempo")
+print("duracion "+str(time.time()-tiempoListaUno));
+print("\n")
+print("\n")
+
+# Método de ordenamiento Inserción
+tiempoListaUno=time.time()
+def ordenamientoPorInsercion(unaLista):
+   for indice in range(1,len(unaLista)):
+
+     valorActual = unaLista[indice]
+     posicion = indice
+
+     while posicion>0 and unaLista[posicion-1]>valorActual:
+         unaLista[posicion]=unaLista[posicion-1]
+         posicion = posicion-1
+     unaLista[posicion]=valorActual
+
+ordenamientoPorInsercion(listaTres)
+print("El orden en la lista por el método de inserción es:"+str(listaTres))
+print("\n")
+print("tiempo")
+print("duracion "+str(time.time()-tiempoListaUno));
+print("\n")
+print("\n")
+
+# Método de ordenamiento Quicksort
+tiempoListaUno=time.time()
+def sort(lista):
+    izquierda = []
+    centro = []
+    derecha = []
+    if len(lista) > 1:
+        pivote = lista[0]
+        for i in lista:
+            if i < pivote:
+                izquierda.append(i)
+            elif i == pivote:
+                centro.append(i)
+            elif i > pivote:
+                derecha.append(i)
+        #print(izquierda+["-"]+centro+["-"]+derecha)
+        return sort(izquierda)+centro+sort(derecha)
+    else:
+      return lista
+
+print()
+print ("El orden en la lista por el método de Quicksort es:"+str(sort(listaTres)))
+print("tiempo")
+print("duracion "+str(time.time()-tiempoListaUno));
+print("\n")
+print("\n")
